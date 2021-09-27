969b453528836b3d881cc1b6ab9ab34ebf3038a8
Eugeny Bobylev <bea@hotbox.ru>
2021-09-27 07:59:16 +0300

    added file Readme.txt

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+https://www.companywall.rs/pretraga?cr=RSD&n=&mv=&r=&c=&FromDateAlt=&FromDate=&ToDateAlt=&ToDate=&at=9602&area=&subarea=&sbjact=t&type=&hr=&dsm%5B0%5D.Code=1101&dsm%5B1%5D.Code=966&dsm%5B-1%5D.Code=0&dsm%5B-1%5D.From=0&dsm%5B-1%5D.To=0&xpnd=true HTTP/1.1
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import requests
+from requests import Response
+
+
+def save_html(html: str, fn: str):
+    if html:
+        with open(fn, 'w', encoding='utf-8') as f:
+            f.write(html)
+
+
+def get_url(url: str) -> Response:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0'}
+    _response = requests.get(url,headers=headers)
+    return _response
+
 
 if __name__ == '__main__':
-    print('привет python')
+    url = 'https://www.companywall.rs/'
+    response = get_url(url)
+
+    print(f'status = {response.status_code}')
+    print(len(response.text))
+
+    if response.status_code == 200:
+        save_html(response.text, 'data/main.html')
