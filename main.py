5bb91e4e6662f0739101123bc34be7c4bd4b0edd
Eugeny Bobylev <bea@hotbox.ru>
2021-09-27 09:20:18 +0300

    work get_company_by_code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_company_by_code(code: str) -> Response:
     _url = f'https://www.companywall.rs/pretraga?cr=RSD&n=&mv=&r=&c=&FromDateAlt=&FromDate=&ToDateAlt=' \
            f'&ToDate=&at={code}&area=&subarea=&sbjact=t&type=&hr=&dsm%5B0%5D.Code=1101&dsm%5B1%5D.' \
            f'Code=966&dsm%5B-1%5D.Code=0&dsm%5B-1%5D.From=0&dsm%5B-1%5D.To=0&xpnd=true HTTP/1.1'
-    _response = get_url(url)
+    _response = get_url(_url)
     return _response
 
 
